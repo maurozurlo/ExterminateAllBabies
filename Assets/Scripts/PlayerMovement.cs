ef15917d39e4171ee7d0754078b5a53500676c90
unknown <mauro.e.zurlo@gmail.com>
2021-03-07 22:52:30 -0300

    First working version on mobile

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float displacementMax, displacementMin;
+    public float maxX, minX, maxY, minY;
+    public float speed;
+    public bool hitting;
+    public bool dead;
+    public float secondsToWait;
+    public Sprite frozenMaterial, flashMaterial;
+    Sprite spriteOrg;
+    public GameObject wasp;
+    AudioSource AS;
+    public AudioClip swat;
+
+    void Start()
+    {
+        AS = this.gameObject.AddComponent<AudioSource>();
+        if (accountForLevels.control != null)
+        {
+            AS.volume = accountForLevels.control.fxVolume;
+        }
+        spriteOrg = this.GetComponentInChildren<SpriteRenderer>().sprite;
+    }
+
+    void Update()
+    {
+        //TODO find out if this works on mobile	
+        //hor/ver should be CrossPlatformInputManager.GetAxis ("Horizontal")?
+        float hor = -CrossPlatformInputManager.VirtualAxisReference("Horizontal").GetValue;
+        float ver = CrossPlatformInputManager.VirtualAxisReference("Vertical").GetValue;
+
+        Vector3 movementX = new Vector3(hor, 0, 0) * Time.deltaTime * speed;
+        Vector3 movementY = new Vector3(0, ver, 0) * Time.deltaTime * speed;
+        if (gameController.control.currentState != gameController.states.paused)
+        {
+            if (!dead)
+            {
+                if (isWithinRange(transform.position.x + movementX.x, minX, maxX))
+                {
+                    this.transform.position += movementX;
+                }
+
+                if (isWithinRange(transform.position.y + movementY.y, minY, maxY))
+                {
+                    this.transform.position += movementY;
+                }
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+					StartHitting();
+                }
+            }
+        }
+    }
+
+    public void StartHitting()
+    {
+        if (!hitting)
+        StartCoroutine("Hit");
+    }
+
+    public bool isWithinRange(float val, float min, float max)
+    {
+        if (val >= min && val <= max)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
+    IEnumerator Hit()
+    {
+        hitting = true;
+        gameController.control.currentState = gameController.states.hitting;
+        wasp.GetComponent<Animator>().SetBool("hit", true);
+        if (!AS.isPlaying)
+        {
+            AS.PlayOneShot(swat);
+        }
+        yield return new WaitForSeconds(secondsToWait);
+        wasp.GetComponent<Animator>().SetBool("hit", false);
+        yield return new WaitForSeconds(secondsToWait);
+        hitting = false;
+        gameController.control.currentState = gameController.states.idle;
+    }
+
+
+    public void Die()
+    {
+        dead = true;
+        gameController.control.currentState = gameController.states.dead;
+        //Mostrarte la screen de q te moriste
+    }
+
+
+    public void returnVisualsToNormal()
+    {
+        this.GetComponentInChildren<SpriteRenderer>().sprite = spriteOrg;
+    }
+
+    public void changeVisuals(string mySkin)
+    {
+        switch (mySkin)
+        {
+            case "ice":
+                this.GetComponentInChildren<SpriteRenderer>().sprite = frozenMaterial;
+                break;
+            case "flash":
+                this.GetComponentInChildren<SpriteRenderer>().sprite = flashMaterial;
+                break;
+        }
+    }
+}
--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -4,246 +4,284 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class gameController : MonoBehaviour {
-	public enum states
-	{
-		juststarted, idle, hitting, dead, paused
-	};
-	public states currentState;
-	public Text scoreText;
-	public Text babyText;
-	public Text pauseText, cuentaRegresiva;
-	public Image damageScreen;
-	public Button menuBut;
+public class gameController : MonoBehaviour
+{
+    public enum states
+    {
+        juststarted, idle, hitting, dead, paused
+    };
+    public states currentState;
+    public Text scoreText;
+    public Text babyText;
+    public Text pauseText, cuentaRegresiva;
+    public Image damageScreen;
+    public Button menuBut;
 
-	public bool paused;
-	public int babySpeedForThisLevel = 3;
-	public int scoreInt;
-	public int babyInt;
-	public int totalBabies;
-	public float speedMultiplier;
-	public static gameController control;
-	public GameObject babyControl;
-	public float delay;
-	public int howManyInARow;
-	AudioSource AS;
-	public AudioClip[] hurtSound;
-	public AudioClip youWon, bombSound, snifSound;
-	public bool upsideDownWorld;
-
-
-
-	[Header("Power Up Stuff")]
-
-	public float powerUpTime = 2;
-	public enum powerUpActivated
-	{
-		none, flash, bomb, lsd, ice
-	}
-	public powerUpActivated powerUp;
-	public GameObject lsdCamera;
-	GameObject player, music;
-	float orgSpeedForMouse;
-	public float frozenMouseSpeed, flashMouseSpeed;
-
-	// Use this for initialization
-	void Awake () {
-		if (control == null) {
-			control = this;
-		} else {
-			Destroy (this.gameObject);
-		}
-		this.currentState = states.juststarted;
-		UpdateUI ();
-		StartCoroutine ("SetUpStuff");
-		AS = this.gameObject.AddComponent<AudioSource> ();
-		if (accountForLevels.control != null) {
-			AS.volume = accountForLevels.control.fxVolume;
-		}
-
-		player = GameObject.Find ("Player");
-		music = GameObject.Find ("Music");
-		orgSpeedForMouse = player.GetComponent<MouseMove> ().speed;
-		Time.timeScale = 1.0f;
-	}
-
-	void Update(){
-		if (currentState != states.juststarted) {
-			if (Input.GetKeyDown (KeyCode.Escape)) {
-				if (!paused) {
-					Pause ();
-					paused = true;
-					//cambiar UI
-					pauseText.gameObject.SetActive (true);
-					menuBut.gameObject.SetActive (true);
-				} else {
-					Resume ();
-					paused = false;
-					pauseText.gameObject.SetActive (false);
-					menuBut.gameObject.SetActive (false);
-					//cambiar UI
-				}
-			}
-		}
-	}
-
-	// Update is called once per frame
-	public void UpdateUI () {
-		babyText.text = "/ " + babyInt.ToString ();
-		scoreText.text = " = " + scoreInt.ToString ();
-	}
+    public bool paused;
+    public int babySpeedForThisLevel = 3;
+    public int scoreInt;
+    public int babyInt;
+    public int totalBabies;
+    public float speedMultiplier;
+    public static gameController control;
+    public GameObject babyControl;
+    public float delay;
+    public int howManyInARow;
+    AudioSource AS;
+    public AudioClip[] hurtSound;
+    public AudioClip youWon, bombSound, snifSound;
+    public bool upsideDownWorld;
 
 
-	IEnumerator SetUpStuff(){
-		for (int i = 0; i < delay; i++) {
-			cuentaRegresiva.text = (delay - i).ToString ();
-			yield return new WaitForSeconds (1);
-		}
-		cuentaRegresiva.text = 0.ToString();
-		yield return new WaitForSeconds (1);
-		cuentaRegresiva.text = "";
-		babyControl.GetComponent<spawnBabies> ().enabled = true;
-		babyControl.GetComponent<spawnPowerUps> ().enabled = true;
-		currentState = states.idle;
-		InvokeRepeating ("IncreaseSpeed", 0, 5);
-	}
 
-	public void Pause(){
-		currentState = states.paused;
-		babyControl.GetComponent<spawnBabies> ().halt ();
-		babyControl.GetComponent<spawnPowerUps> ().halt ();
+    [Header("Power Up Stuff")]
 
-		GameObject[] babiesNow = GameObject.FindGameObjectsWithTag ("BabyEnemy");
-		foreach (var item in babiesNow) {
-			item.GetComponent<Baby> ().kill ();
-			item.GetComponentInChildren<BabyAnimation>().alive = false;
-		}
+    public float powerUpTime = 2;
+    public enum powerUpActivated
+    {
+        none, flash, bomb, lsd, ice
+    }
+    public powerUpActivated powerUp;
+    public GameObject lsdCamera;
+    GameObject player, music;
+    float orgSpeedForMouse;
+    public float frozenMouseSpeed, flashMouseSpeed;
 
-		GameObject[] pUps = GameObject.FindGameObjectsWithTag ("PowerUp");
-		foreach (var item in pUps) {
-			item.GetComponent<genericPowerUp> ().PausePU ();
-		}
+    // Use this for initialization
+    void Awake()
+    {
+        if (control == null)
+        {
+            control = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        this.currentState = states.juststarted;
+        UpdateUI();
+        StartCoroutine("SetUpStuff");
+        AS = this.gameObject.AddComponent<AudioSource>();
+        if (accountForLevels.control != null)
+        {
+            AS.volume = accountForLevels.control.fxVolume;
+        }
 
-	}
+        player = GameObject.Find("Player");
+        music = GameObject.Find("Music");
+        orgSpeedForMouse = player.GetComponent<PlayerMovement>().speed;
+        Time.timeScale = 1.0f;
+    }
 
-	public void Resume(){
-		currentState = states.idle;
-		babyControl.GetComponent<spawnBabies> ().resume ();
-		GameObject[] babiesNow = GameObject.FindGameObjectsWithTag ("BabyEnemy");
-		foreach (var item in babiesNow) {
-			item.GetComponent<Baby> ().revive ();
-			item.GetComponentInChildren<BabyAnimation>().alive = true;
-		}
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            PauseRoutine();
+    }
 
-		babyControl.GetComponent<spawnPowerUps> ().resume ();
-		GameObject[] pUps = GameObject.FindGameObjectsWithTag ("PowerUp");
-		foreach (var item in pUps) {
-			item.GetComponent<genericPowerUp> ().revive ();
-		}
+    // Update is called once per frame
+    public void UpdateUI()
+    {
+        babyText.text = "/ " + babyInt.ToString();
+        scoreText.text = " = " + scoreInt.ToString();
+    }
 
-	}
-
-	public IEnumerator takeDamage(int dmgToTake, Color col){
-		scoreInt = scoreInt - dmgToTake;
-		damageScreen.color = col;
-
-		if (scoreInt > -1) {
-			UpdateUI();
-			howManyInARow = 0;
-			if (!AS.isPlaying) {
-				AS.PlayOneShot (hurtSound [Random.Range(0, hurtSound.Length)]);
-			}
-			damageScreen.gameObject.SetActive (true);
-			damageScreen.GetComponent<ImgHandlers> ().replayAnim ();
-			yield return new WaitForSeconds (1);
-			damageScreen.gameObject.SetActive (false);
-		} else {
-			//game over
-			currentState = states.dead;
-			SceneManager.LoadScene("GameOver");
-		}
-	}
-
-
-	public void WeWon(){
-		StartCoroutine("WinRoutine");
-	}
-
-	IEnumerator WinRoutine(){
-		cuentaRegresiva.text = "YOU WIN";
-		AS.PlayOneShot (youWon);
-		yield return new WaitForSeconds (youWon.length + 1);
-		SceneManager.LoadScene ("YouWon");
-	}
-
-	public void StopPowerUps(){
-		babyControl.GetComponent<spawnPowerUps> ().halt ();
-	}
-
-	public void IncreaseSpeed(){
-		babyControl.GetComponent<spawnBabies> ().IncreaseSpeed (speedMultiplier);
-	}
+    public void PauseRoutine()
+    {
+        if (currentState != states.juststarted)
+        {
+            if (!paused)
+            {
+                Pause();
+                paused = true;
+                pauseText.gameObject.SetActive(true);
+                menuBut.gameObject.SetActive(true);
+            }
+            else
+            {
+                Resume();
+                paused = false;
+                pauseText.gameObject.SetActive(false);
+                menuBut.gameObject.SetActive(false);
+            }
+        }
+    }
 
 
-	public void activatePowerUp(powerUpActivated pw, int myDamage){
-		int damageForThisLevel;
+    IEnumerator SetUpStuff()
+    {
+        for (int i = 0; i < delay; i++)
+        {
+            cuentaRegresiva.text = (delay - i).ToString();
+            yield return new WaitForSeconds(1);
+        }
+        cuentaRegresiva.text = 0.ToString();
+        yield return new WaitForSeconds(1);
+        cuentaRegresiva.text = "";
+        babyControl.GetComponent<spawnBabies>().enabled = true;
+        babyControl.GetComponent<spawnPowerUps>().enabled = true;
+        currentState = states.idle;
+        InvokeRepeating("IncreaseSpeed", 0, 5);
+    }
+
+    public void Pause()
+    {
+        currentState = states.paused;
+        babyControl.GetComponent<spawnBabies>().halt();
+        babyControl.GetComponent<spawnPowerUps>().halt();
+
+        GameObject[] babiesNow = GameObject.FindGameObjectsWithTag("BabyEnemy");
+        foreach (var item in babiesNow)
+        {
+            item.GetComponent<Baby>().kill();
+            item.GetComponentInChildren<BabyAnimation>().alive = false;
+        }
+
+        GameObject[] pUps = GameObject.FindGameObjectsWithTag("PowerUp");
+        foreach (var item in pUps)
+        {
+            item.GetComponent<genericPowerUp>().PausePU();
+        }
+
+    }
+
+    public void Resume()
+    {
+        currentState = states.idle;
+        babyControl.GetComponent<spawnBabies>().resume();
+        GameObject[] babiesNow = GameObject.FindGameObjectsWithTag("BabyEnemy");
+        foreach (var item in babiesNow)
+        {
+            item.GetComponent<Baby>().revive();
+            item.GetComponentInChildren<BabyAnimation>().alive = true;
+        }
+
+        babyControl.GetComponent<spawnPowerUps>().resume();
+        GameObject[] pUps = GameObject.FindGameObjectsWithTag("PowerUp");
+        foreach (var item in pUps)
+        {
+            item.GetComponent<genericPowerUp>().revive();
+        }
+
+    }
+
+    public IEnumerator takeDamage(int dmgToTake, Color col)
+    {
+        scoreInt = scoreInt - dmgToTake;
+        damageScreen.color = col;
+
+        if (scoreInt > -1)
+        {
+            UpdateUI();
+            howManyInARow = 0;
+            if (!AS.isPlaying)
+            {
+                AS.PlayOneShot(hurtSound[Random.Range(0, hurtSound.Length)]);
+            }
+            damageScreen.gameObject.SetActive(true);
+            damageScreen.GetComponent<ImgHandlers>().replayAnim();
+            yield return new WaitForSeconds(1);
+            damageScreen.gameObject.SetActive(false);
+        }
+        else
+        {
+            //game over
+            currentState = states.dead;
+            SceneManager.LoadScene("GameOver");
+        }
+    }
 
 
-		if (accountForLevels.control != null) {
-			damageForThisLevel = myDamage * accountForLevels.control.currentLevel;
-		} else {
-			damageForThisLevel = myDamage;
-		}
+    public void WeWon()
+    {
+        StartCoroutine("WinRoutine");
+    }
 
-		if (pw != powerUpActivated.flash) {
-			
+    IEnumerator WinRoutine()
+    {
+        cuentaRegresiva.text = "YOU WIN";
+        AS.PlayOneShot(youWon);
+        yield return new WaitForSeconds(youWon.length + 1);
+        SceneManager.LoadScene("YouWon");
+    }
 
-		}
+    public void StopPowerUps()
+    {
+        babyControl.GetComponent<spawnPowerUps>().halt();
+    }
 
-		switch (pw) {
-		case powerUpActivated.bomb:
-			Debug.Log ("got hit by bomb");
-			StartCoroutine (gameController.control.takeDamage (damageForThisLevel,Color.red));
-			AS.PlayOneShot (bombSound);
-			break;
-		case powerUpActivated.flash:
-			Debug.Log ("got hit by flash");
-			StartCoroutine (gameController.control.takeDamage (damageForThisLevel,Color.yellow));
-			player.GetComponent<MouseMove> ().speed = flashMouseSpeed;
-			player.GetComponent<MouseMove> ().changeVisuals("flash");
-			music.GetComponent<AudioSource> ().pitch = 2;
-			StartCoroutine ("returnPlayerToNormal");
-			break;
-		case powerUpActivated.ice:
-			StartCoroutine (gameController.control.takeDamage (damageForThisLevel,Color.cyan));
-			player.GetComponent<MouseMove> ().speed = frozenMouseSpeed;
-			player.GetComponent<MouseMove> ().changeVisuals("ice");
-			music.GetComponent<AudioSource> ().pitch = .5f;
-			Debug.Log ("got hit by ice");
-			StartCoroutine ("returnPlayerToNormal");
-			break;
-		case powerUpActivated.lsd:
-			Debug.Log ("got hit by lsd");
-			StartCoroutine (gameController.control.takeDamage (damageForThisLevel, Color.magenta));
-			Instantiate (lsdCamera, new Vector3 (0, 4.2f, -0.24f), Quaternion.identity);
-			music.GetComponent<AudioSource> ().pitch = 5;
-			AS.PlayOneShot (snifSound);
-			break;
-		}
-	}
+    public void IncreaseSpeed()
+    {
+        babyControl.GetComponent<spawnBabies>().IncreaseSpeed(speedMultiplier);
+    }
 
 
-	IEnumerator returnPlayerToNormal()
-	{
-		yield return new WaitForSeconds (powerUpTime);
-		music.GetComponent<AudioSource> ().pitch = 1;
-		player.GetComponent<MouseMove>().speed = orgSpeedForMouse;
-		player.GetComponent<MouseMove> ().returnVisualsToNormal ();
-	}
+    public void activatePowerUp(powerUpActivated pw, int myDamage)
+    {
+        int damageForThisLevel;
 
-	public void  returnMusicToNormal(){
-		music.GetComponent<AudioSource> ().pitch = 1;
-	}
+
+        if (accountForLevels.control != null)
+        {
+            damageForThisLevel = myDamage * accountForLevels.control.currentLevel;
+        }
+        else
+        {
+            damageForThisLevel = myDamage;
+        }
+
+        if (pw != powerUpActivated.flash)
+        {
+
+
+        }
+
+        switch (pw)
+        {
+            case powerUpActivated.bomb:
+                Debug.Log("got hit by bomb");
+                StartCoroutine(gameController.control.takeDamage(damageForThisLevel, Color.red));
+                AS.PlayOneShot(bombSound);
+                break;
+            case powerUpActivated.flash:
+                Debug.Log("got hit by flash");
+                StartCoroutine(gameController.control.takeDamage(damageForThisLevel, Color.yellow));
+                player.GetComponent<PlayerMovement>().speed = flashMouseSpeed;
+                player.GetComponent<PlayerMovement>().changeVisuals("flash");
+                music.GetComponent<AudioSource>().pitch = 2;
+                StartCoroutine("returnPlayerToNormal");
+                break;
+            case powerUpActivated.ice:
+                StartCoroutine(gameController.control.takeDamage(damageForThisLevel, Color.cyan));
+                player.GetComponent<PlayerMovement>().speed = frozenMouseSpeed;
+                player.GetComponent<PlayerMovement>().changeVisuals("ice");
+                music.GetComponent<AudioSource>().pitch = .5f;
+                Debug.Log("got hit by ice");
+                StartCoroutine("returnPlayerToNormal");
+                break;
+            case powerUpActivated.lsd:
+                Debug.Log("got hit by lsd");
+                StartCoroutine(gameController.control.takeDamage(damageForThisLevel, Color.magenta));
+                Instantiate(lsdCamera, new Vector3(0, 4.2f, -0.24f), Quaternion.identity);
+                music.GetComponent<AudioSource>().pitch = 5;
+                AS.PlayOneShot(snifSound);
+                break;
+        }
+    }
+
+
+    IEnumerator returnPlayerToNormal()
+    {
+        yield return new WaitForSeconds(powerUpTime);
+        music.GetComponent<AudioSource>().pitch = 1;
+        player.GetComponent<PlayerMovement>().speed = orgSpeedForMouse;
+        player.GetComponent<PlayerMovement>().returnVisualsToNormal();
+    }
+
+    public void returnMusicToNormal()
+    {
+        music.GetComponent<AudioSource>().pitch = 1;
+    }
 
 
 }
